dcffc8c811e44ce894303716c77b935beb9f680e
Elias M. Juan <juaneliasm98@gmail.com>
2024-06-08 09:55:26 -0300

    Realizo los ejercicios 1 y 2

---
+++ js/ejercicio1.js
@@ -0,0 +1,13 @@
+// EJERCICIO 1  --- CONVERSION DE TEMPERATURAS.
+
+let temperaturaActual=7
+console.log(`La temperatura actual es ${temperaturaActual}°C (Celsius).`)
+
+let coeficienteA=1.8
+let coeficienteB=32
+
+Fahrenheit= temperaturaActual*coeficienteA+coeficienteB
+console.log(`La temperatura actual es ${Fahrenheit}°F (Fahrenheit)`)
+
+let temperaturaFahrenheit=44.6
+temperaturaFahrenheit>85? console.log('Temperatura alta.') : console.log ('Temperatura moderada.')
---
+++ js/ejercicio2.js
@@ -0,0 +1,11 @@
+// EJERCICIO 2 --- CALCULO IMC
+
+let peso=130
+let altura=1.89
+console.log(`La persona pesa ${peso}kg y su altura es ${altura}m.`)
+
+imc=peso/(altura*altura)
+console.log(`El indice de masa corporal de la persona es ${imc}.`)
+
+let indice=36.39
+indice>=30? console.log(`Clasificacion IMC: Obesidad`) : console.log(`Clasificacion IMC: Sobrepeso`)
