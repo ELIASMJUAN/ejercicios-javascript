80a190efc77a9c44d403f1c37bd9b8b0cb589680
Elias M. Juan <juaneliasm98@gmail.com>
2024-06-08 11:41:26 -0300

    inicio ejercicio 3 y 4 sin concluirlos

---
+++ js/ejercicio3.js
@@ -0,0 +1,6 @@
+//EJERCICIO 3 --- DETERMINACION DE NUMEROS PRIMOS
+
+let numeroEntero=32
+
+let primo= 1==32
+while (primo =32){console.log(32,primo);
---
+++ js/ejercicio4.js
@@ -0,0 +1,17 @@
+// EJERCICIO 4 --- OPERACIONES ARITMETICAS BASICAS
+
+let numeroA=35
+let numeroB=7
+let numeroC=89
+
+console.log(`Operaciones aritmeticass basicas entre los números ${numeroA}, ${numeroB} y ${numeroC}.`)
+
+resultadoSuma=numeroA+numeroB+numeroC
+resultadoResta=numeroA-numeroB-numeroC
+resultadoMultiplicacion=numeroA*numeroB*numeroC
+
+console.log(`El resultado de la SUMA es ${resultadoSuma}`)
+console.log(`El resultado de la RESTA es ${resultadoResta}`)
+console.log(`El resultado de la MULTIPLICACION es ${resultadoMultiplicacion}`)
+
+if(resultadoSuma>=resultadoResta){console.log()}
