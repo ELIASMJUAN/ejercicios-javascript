267e274959cd79ef9048a4fdddf427fef1ec85cc
Elias M. Juan <juaneliasm98@gmail.com>
2024-06-08 12:08:51 -0300

    realizo ejercicio 3

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,6 +1,19 @@
 //EJERCICIO 3 --- DETERMINACION DE NUMEROS PRIMOS
 
 let numeroEntero=32
-
 let primo= 1==32
-while (primo =32){console.log(32,primo);
+
+function esPrimo(numeroEntero){
+    if(numeroEntero<=1)return false;
+    for(let i=2; i<numeroEntero; i++){
+        if(numeroEntero % i===0){
+            return false;
+        }
+    }
+    return true
+}
+if(esPrimo(numeroEntero)){
+    console.log(numeroEntero+` es un numero primo.`)
+}else{
+    console.log(numeroEntero+` no es un numero primo.`)
+}
